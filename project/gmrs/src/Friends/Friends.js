7ab2961f747e6dba909838e5bcaaec68e3882c9e
yucpu <985968134@qq.com>
2023-04-12 10:32:22 +0800

    change UI

--- project/gmrs/src/Friends/Friends.js
+++ project/gmrs/src/Friends/Friends.js
@@ -1,12 +1,12 @@
 import { Divider } from 'antd'
-import React, { useReducer } from 'react'
+import React, { useMemo, useReducer } from 'react'
 import './Friends.css'
 import { IconButton, InputBase, Paper} from '@mui/material'
 import  SearchIcon from '@mui/icons-material/Search';
-import { Space, Tag } from 'antd';
+import { Space, Tag, List, Avatar} from 'antd';
 const {CheckableTag} = Tag
-let prototype = {'All':false,'id':false,'region':false,'game':false}
-let tags = {'All':true,'id':false,'region':false,'game':false}
+let prototype = {'Friends':false,'id':false,'region':false,'game':false}
+let tags = {'Friends':true,'id':false,'region':false,'game':false}
 
 const reducer = (data, action)=>{
     switch(action.type){
@@ -39,51 +39,69 @@ export default function Friends() {
         console.log(data.query,data.filter)
     }
 
-    return (
-        <div className='profile_firend_layout'>
-            <div id='friendList'>
-                <div>
-                    <Paper  component='form' sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: "100%", height:38 }}>
-                        <InputBase 
-                            sx={{ ml: 1, flex: 1,}}
-                            placeholder="Search Friends"
-                            value={data.query}
-                            onChange={(event)=>handleChange(event,"query")}
-                            
+    return useMemo(()=>{
+        return (
+            <div className='profile_friend_layout'>
+                <div id='friendList'>
+                    <div>
+                        <Paper  component='form' sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: "100%", height:38 }}>
+                            <InputBase 
+                                sx={{ ml: 1, flex: 1,}}
+                                placeholder="Search Friends"
+                                value={data.query}
+                                onChange={(event)=>handleChange(event,"query")}
+                                
+                            />
+                            <IconButton type="button" sx={{ p: '10px' }} aria-label="search" onClick={handleSearch}>
+                                <SearchIcon />
+                            </IconButton>
+                        </Paper>
+                        <div style={{display:"flex"}}>
+                            <span
+                                style={{
+                                marginRight: 8,
+                                }}
+                            >
+                                Filter:
+                            </span>
+                            <Space size={[10, 10]} wrap>
+                                {Object.keys(data.tags).map((item)=>(
+                                    <CheckableTag
+                                        key={item}
+                                        checked={data.tags[item]}
+                                        onChange={()=>{handleChange(null,'tags',item)}}
+                                    >
+                                        {item}
+                                    </CheckableTag>
+                                ))}
+                            </Space>
+                        </div>
+                    </div>
+                    <Divider></Divider>
+                    <div>
+                        <List
+                            itemLayout="horizontal"
+                            dataSource={[]}
+                            renderItem={(item, index) => (
+                            <List.Item>
+                                <List.Item.Meta
+                                avatar={<Avatar src={`https://xsgames.co/randomusers/avatar.php?g=pixel&key=${index}`} />}
+                                title={<a href="https://ant.design">{item.title}</a>}
+                                description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+                                />
+                            </List.Item>
+                            )}
                         />
-                        <IconButton type="button" sx={{ p: '10px' }} aria-label="search" onClick={handleSearch}>
-                            <SearchIcon />
-                        </IconButton>
-                    </Paper>
-                    <div style={{display:"flex"}}>
-                        <span
-                            style={{
-                            marginRight: 8,
-                            }}
-                        >
-                            Filter:
-                        </span>
-                        <Space size={[10, 10]} wrap>
-                            {Object.keys(data.tags).map((item)=>(
-                                <CheckableTag
-                                    key={item}
-                                    checked={data.tags[item]}
-                                    onChange={()=>{handleChange(null,'tags',item)}}
-                                >
-                                    {item}
-                                </CheckableTag>
-                            ))}
-                        </Space>
                     </div>
                 </div>
-                <div>
-                    List
+                <Divider type='vertical' style={{height:'100%'}}/>
+                <div id='friendInfo'>
+                    info
                 </div>
             </div>
-            <Divider type='vertical'/>
-            <div id='friendInfo'>
-                info
-            </div>
-        </div>
+        )
+        },
+        [data]
     )
+
 }
