ead6150b3f90705b9160e5da54b6fcbe11224587
yucpu <985968134@qq.com>
2023-04-13 03:20:42 +0800

    retrieve data from backend

--- project/gmrs/src/Friends/Friends.js
+++ project/gmrs/src/Friends/Friends.js
@@ -1,44 +1,56 @@
-import { Divider } from 'antd'
-import React, { useMemo, useReducer } from 'react'
+import { Button, Divider } from 'antd'
+import React, { useEffect, useMemo, useReducer } from 'react'
 import './Friends.css'
 import { IconButton, InputBase, Paper} from '@mui/material'
 import  SearchIcon from '@mui/icons-material/Search';
 import { Space, Tag, List, Avatar} from 'antd';
+import { getData, useData } from '../util/data';
 const {CheckableTag} = Tag
-let prototype = {'Friends':false,'id':false,'region':false,'game':false}
-let tags = {'Friends':true,'id':false,'region':false,'game':false}
-
+let prototype = {'friends':false,'id':false,'region':false,'game':false}
+let tags = {'friends':true,'id':false,'region':false,'game':false}
+let info = ['nickname', 'age', 'region', 'email', 'Games'];
 const reducer = (data, action)=>{
     switch(action.type){
         case "query":
             return {...data, query:action.value}
         case "filter":
-            return {...data, query:action.value}
+            return {...data, filter:action.value}
         case "friendList":
-            return {...data, query:action.value}
+            return {...data, friendList:action.value}
         case "tags":
             return {...data, tags:action.value}
+        case "selected":
+            return {...data, selected:action.value}
         default:
             return {...data}
     }
 }
 
 export default function Friends() {
-    const [data, dispatch] = useReducer(reducer, {query:"", filter:0, friendList:[],tags:tags})
+    let context = useData();
+    const [data, dispatch] = useReducer(reducer, {query:"", filter:"friends", friendList:[],tags:tags, selected:null})
     
+    useEffect(()=>{
+        getData("friends",{id:124,filter:data.filter,query:data.query}).then((res)=>dispatch({type:"friendList",value:res.friendList}))
+    },[])
+
 
     let handleChange=(event, type, value) =>{
         if(type == "tags"){
             let selected = {...prototype, [value]:true}
+            dispatch({type:"filter", value: value})
             return dispatch({type:type,value:selected})
         }
         dispatch({type:type, value:value? value:event.target.value})
     }
 
     let handleSearch = () =>{
-        console.log(data.query,data.filter)
+        getData("friends",{id:124,filter:data.filter,query:data.query}).then((res)=>dispatch({type:"friendList",value:res.friendList}))
     }
 
+    
+
+ 
     return useMemo(()=>{
         return (
             <div className='profile_friend_layout'>
@@ -81,13 +93,13 @@ export default function Friends() {
                     <div>
                         <List
                             itemLayout="horizontal"
-                            dataSource={[]}
+                            dataSource={data.friendList}
                             renderItem={(item, index) => (
                             <List.Item>
                                 <List.Item.Meta
                                 avatar={<Avatar src={`https://xsgames.co/randomusers/avatar.php?g=pixel&key=${index}`} />}
-                                title={<a href="https://ant.design">{item.title}</a>}
-                                description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+                                title={<a onClick={()=>dispatch({type:"selected",value:item.user})}>{item.user.nickname}</a>}
+                                description={item.user.userID}
                                 />
                             </List.Item>
                             )}
@@ -95,9 +107,29 @@ export default function Friends() {
                     </div>
                 </div>
                 <Divider type='vertical' style={{height:'100%'}}/>
-                <div id='friendInfo'>
-                    info
-                </div>
+                {
+                    data.selected ? 
+                        <div id='friendInfo'>
+                            <img src={require("../resources/gamer_1.png")} width="20%"/>
+                            {info.map((itme)=>(
+                                <div key={`option-${itme}`} style={{display:'flex', flexDirection:'row', justifyContent:'space-between', width:'80%'}}>
+                                    <div>
+                                        {itme}
+                                    </div>
+                                    <div>
+                                        {
+                                            data.selected[itme]
+                                        }
+                                    </div>
+                                </div>
+                            ))}
+                            <Button>Remove </Button>
+                        </div>
+                        :
+                        <div>
+                        </div>
+                }
+                
             </div>
         )
         },
